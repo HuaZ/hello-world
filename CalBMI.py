d9241bdc7550bc7f985308e2d98c0ef62ed41f76
Hua Zhu <hua.zhu@ericsson.com>
2018-10-09 22:04:34 +0800

    PA1.0

---
+++ CalBMI.py
@@ -0,0 +1,10 @@
+#CalBMI.py
+height, weight = eval(input("请输入身高（米）和体重（公斤）【逗号隔开】："))
+bmi = weight / pow(height, 2)
+print("BMI 数值：{:.2f}".format(bmi))
+who, nat = "", ""
+if bmi < 18.5:
+    who, nat = "偏瘦", "偏瘦"
+elif 18.5 <= bmi < 24:
+    who, nat = "正常", "正常"
+print("BMI指标为：国际{},国内{}".format(who, nat))
